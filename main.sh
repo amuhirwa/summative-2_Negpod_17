d3bdcae0445cc30afb075ecd6f27317354ff91f1
amuhirwa <a.muhirwa@alustudent.com>
2023-10-09 04:01:29 -0700

    Fixed update

--- main.sh
+++ main.sh
@@ -6,10 +6,16 @@ create_rec() {
 	read -rp "Enter your student email : " student_email
 	read -rp "Enter your age : " student_age
 	read -rp "Enter your student ID : " student_id
+	s_email=$(echo $student_email | tr -d ' ')
+	s_age=$(echo $student_age | tr -d ' ')
+	s_id=$(echo $student_id | tr -d ' ')
+	echo "Student record created successfully"
+	echo "Choose 'output' option to save record to file"
 }
 # Function to save records to students-list file
 output_to_file() {
-	echo -e "Student ID: $student_id\tStudent email: $student_email\tStudent age: $student_age" >> "students-list_0923.txt"
+	echo -e "Student ID: $s_id\tStudent email: $s_email\tStudent age: $s_age" >> "students-list_0923.txt"
+	echo "Record saved to file successfully"
 }
 # Function to view students saved in students-list file
 view_recs() {
@@ -23,14 +29,27 @@ view_recs() {
 # !!!! ADD OTHER FUNCTIONS HERE
 delete() {
         read -rp "Which student would you like to delete?(Enter student ID) : " delete_id
-        sed -i "/Student ID: $delete_id/d" "students-list_0923.txt"
-}
+	if (( $(grep -cP "Student ID: $delete_id[^[[:digit:]]]*" < "students-list_0923.txt") >= 1))
+	then	
+	        sed -i "/Student ID: $delete_id[^[[:digit:]]]*/d" "students-list_0923.txt"
+		echo "Student $delete_id has been deleted successfully"
+	else
+		echo "Student with student ID: $delete_id not found for deletion"
+	fi
+	}
 
 update() {
         read -rp "Which student would you like to update?(Enter student ID) : " update_id
-        read -rp "Enter new email : " new_email
-        read -rp "Enter new age : " new_age
-        sed -i "s/Student ID: $update_id .*/Student ID: $update_id \tStudent email: $new_email \tStudent age: $new_age/" "students-list_0923.txt"
+	if (( $(grep -cP "Student ID: $update_id[^[[:digit:]]]*" < "students-list_0923.txt") >= 1))
+        then
+        	read -rp "Enter new email : " new_email
+        	read -rp "Enter new age : " new_age
+        	sed -i "s/Student ID: $update_id\t.*/Student ID: $update_id\tStudent email: $new_email\tStudent age: $new_age/" "students-list_0923.txt"
+		echo "Student with student ID: $update_id has been updated successfully"
+	else
+		echo "Student with student ID: $update_id not found to update"
+
+	fi
 }
 exit_app(){
         exit
@@ -50,9 +69,6 @@ do
 		delete) delete ;;
 		update) update ;;
 		exit) exit_app ;;
-
-	
-
 		*) echo "Usage: main.sh {create|output|view|delete|update|exit}" 
 	esac
 done
