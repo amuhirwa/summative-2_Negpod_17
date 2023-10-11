6a34c436af594570555cd06f672b810f8da7fc48
Diana-codes <dianaruzindana8@gmail.com>
2023-10-11 03:53:11 -0700

    select-emails

---
+++ select-emails.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Use cut to extract student emails and save them to student-emails.txt
+grep -oE '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}' students-list_0923.txt > student-emails.txt
+echo "Student emails extracted and saved to student-emails.txt"
+
