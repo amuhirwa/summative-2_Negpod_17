474c348204b9cf9c0966aca3ae7b25d59c723c0d
nmaketh <n.riak@alustudent.com>
2023-10-13 11:00:33 +0200

    nhail

---
+++ select-emails.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#inputing file that contain data with  emails
+input_file="select-emails.sh"
+output_file="student-emails.txt"
+echo "student emails have been well selected and saved in $out_file"
+
