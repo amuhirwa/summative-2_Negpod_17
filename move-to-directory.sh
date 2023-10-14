893e05d41b9f2fe51b3a9fd1771ab7d03b0b207c
nmaketh <n.riak@alustudent.com>
2023-10-14 11:22:36 +0200

    move-to-directory.sh

---
+++ move-to-directory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#creating directory and move four files automatically
+mkdir -p negpod_id-ql
+mv main.sh negpod-id-ql/
+mv Students-list_0923.txt negpod_id-ql/
+mv Select-emails.sh negpod_id-ql/
+mv student-emails.txt negpod_id-ql/
+echo "four files have been moved successfully"
