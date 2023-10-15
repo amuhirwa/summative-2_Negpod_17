1a6101e270054d2598bec7c77351bee9046b0bdd
nmaketh <n.riak@alustudent.com>
2023-10-15 16:03:09 +0200

    move-to-directory.sh

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #shell script to automatically move the all specified files to the "negpod_4-q1" directory
-destination_dir="negpod_4-q1"
+destination_dir="negpod_id-q1"
 
 if [ ! -d "$destination_dir" ]; then
   mkdir "$destination_dir"
@@ -17,13 +17,5 @@ for file in "${files_to_move[@]}"; do
   fi
 done
 
-
-# Create the directory if it doesn't exist before
-mkdir -p negpod_id-q1
-
-# Move the files to the directory
-mv main.sh students-list_0923.txt select-emails.sh student-emails.txt negpod_id-q1
-
-echo "Files moved to negpod_id-q1 directory."
 
 
